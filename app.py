a2a6d1ac815245d822d6f7f452a0531657ed9e0c
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 13:45:35 +0900

    from ○ import を勉強

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-import calculation
+# import calculation
+#
+# if __name__ == '__main__':
+#     nums = [1, 2, 3]
+#     total = calculation.calculation_sum(nums)
+#
+#     print(total)
+
+
+# calculation.pyからcalculation_sumをもってくる
+from calculation import calculation_sum
 
 if __name__ == '__main__':
     nums = [1, 2, 3]
-    total = calculation.calculation_sum(nums)
+    total = calculation_sum(nums)
 
     print(total)
