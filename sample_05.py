2658800b0bb919bc60f42bd829f4323696a15cbf
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 11:39:37 +0900

    sample_05ファイルを作成

--- sample_05.py
+++ sample_05.py
@@ -1,3 +1,4 @@
+# 2つの整数の和を返す関数
 def add(a: int, b: int) -> int:
     return a + b
 
@@ -5,3 +6,13 @@ def add(a: int, b: int) -> int:
 y = add(a=3, b=4)
 
 print(y)
+
+
+# 2つの整数の差を返す関数
+def sub(a: int, b: int) -> int:
+    return a - b
+
+
+z = sub(a=3, b=4)
+
+print(z)
