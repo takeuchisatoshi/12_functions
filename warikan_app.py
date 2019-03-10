314a734ade939aba1e228c9a5ee70893b75b52bc
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 10:32:11 +0900

    割り勘アプリ作成

---
+++ warikan_app.py
@@ -0,0 +1,9 @@
+
+
+amount = int(input("合計金額を入力してね > "))
+number_of_people = int(input("人数を入力してね > "))
+
+payment = amount // number_of_people
+remainder = amount % number_of_people
+
+print(f"1人: {payment}円､ 端数: {remainder}円")
